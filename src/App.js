873a3a5e05767c86932d37aa6f732ac28a6851f4
Booker Ngoon <bookerngoon@gmail.com>
2024-04-17 18:20:35 -0400

    first commits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import {
+  createAction,
+  nanoid,
+  createReducer,
+  configureStore,
+} from "@reduxjs/toolkit";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const initialState = {
+    counter: 0,
+  };
+
+  // Define the increment action with a unique type identifier
+  const increment = createAction("counter/increment");
+  const decrement = createAction("counter/decrement");
+  const resetCounter = createAction("counter/reset");
+  const incrementBy = createAction("counter/incrementBy", (amount, user) => {
+    return {
+      payload: {
+        amount,
+        user,
+        id: nanoid(),
+      },
+    };
+  });
+
+  // Define reducer
+  const counterReducer = createReducer(initialState, (builder) => {
+    builder
+      .addCase(increment, (state) => {
+        state.counter += 1;
+      })
+      .addCase(decrement, (state) => {
+        state.counter -= 1;
+      })
+      .addCase(resetCounter, (state) => {
+        state.counter = 0;
+      })
+      .addCase(incrementBy, (state, action) => {
+        state.counter += action.payload.amount;
+      });
+  });
+
+  // Create store
+  const store = configureStore({
+    reducer: {
+      counter: counterReducer,
+    },
+  });
+
+  // Dispatch actions
+  store.dispatch(increment());
+  store.dispatch(increment());
+  store.dispatch(incrementBy(100, ""));
+  console.log(store.getState());
+
+  return <div className="App">react</div>;
 }
 
 export default App;
